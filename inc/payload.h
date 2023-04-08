956bffca116e8a86d39ca5b936f523a29968d0c1
Viktor Ciolkowski <wikcioo@gmail.com>
2023-04-08 13:13:06 +0200

    Add payload function prototypes

--- inc/payload.h
+++ inc/payload.h
@@ -30,4 +30,11 @@ typedef struct
     int32_t high;
 } range_t;
 
+payload_id_t payload_get_id(uint8_t *data);
+uint8_t *    payload_pack_thc(uint8_t flags, int16_t temperature, uint8_t humidity, uint16_t co2);
+
+void payload_unpack_thc_presets(uint8_t *data, range_t *temperature_range, range_t *humidity_range, range_t *co2_range);
+void payload_unpack_intervals(uint8_t *data, interval_t *intervals, uint8_t *no_intervals);
+void payload_unpack_actions(uint8_t *data, action_t *actions);
+
 #endif /* PAYLOAD_H_ */
