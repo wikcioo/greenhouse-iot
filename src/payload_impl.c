2d8b6fe0019d2aeb7cd64f04f43d90275dad3aaf
Viktor Ciolkowski <wikcioo@gmail.com>
2023-04-15 23:33:28 +0200

    Implement getting id of payload

--- src/payload_impl.c
+++ src/payload_impl.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include "payload.h"
 
+static uint8_t *hex_str_to_u8_ptr(const char *hex_str)
+{
+    size_t   length = strlen(hex_str);
+    uint8_t *data   = (uint8_t *) calloc(length / 2, sizeof(uint8_t));
+
+    for (size_t i = 0; i < length; i += 2)
+    {
+        char    hex_byte[3] = {hex_str[i], hex_str[i + 1], '\0'};
+        uint8_t hex_value;
+        sscanf(hex_byte, "%hhx", &hex_value);
+        data[i / 2] = hex_value;
+    }
+
+    return data;
+}
+
+payload_id_t payload_get_id(const char *hex_str)
+{
+    uint8_t *data = hex_str_to_u8_ptr(hex_str);
+    return (payload_id_t)((data[0] >> 2) & 0x3F);
+}
+
 payload_uplink_t payload_pack_thc(uint8_t flags, int16_t temperature, uint8_t humidity, uint16_t co2)
 {
     uint8_t *payload = (uint8_t *) calloc(6, sizeof(uint8_t));
@@ -24,22 +46,6 @@ payload_uplink_t payload_pack_thc(uint8_t flags, int16_t temperature, uint8_t hu
     payload[5] |= (co2 & 0xF);  // 6th byte += 4bits of co2
 
     return (payload_uplink_t){payload, 6};
-}
-
-static uint8_t *hex_str_to_u8_ptr(const char *hex_str)
-{
-    size_t   length = strlen(hex_str);
-    uint8_t *data   = (uint8_t *) calloc(length / 2, sizeof(uint8_t));
-
-    for (size_t i = 0; i < length; i += 2)
-    {
-        char    hex_byte[3] = {hex_str[i], hex_str[i + 1], '\0'};
-        uint8_t hex_value;
-        sscanf(hex_byte, "%hhx", &hex_value);
-        data[i / 2] = hex_value;
-    }
-
-    return data;
 }
 
 void payload_unpack_thc_presets(const char *hex_str, range_t *temp_range, range_t *hum_range, range_t *co2_range)
