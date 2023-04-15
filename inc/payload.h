a4cde8f29e9397b0dd002883bfb6080650d2cc40
Viktor Ciolkowski <wikcioo@gmail.com>
2023-04-15 23:33:28 +0200

    Implement getting id of payload

--- inc/payload.h
+++ inc/payload.h
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define PAYLOAD_ID_TO_NAME(x) \
+    (x == 1 ? "THC_READINGS" : x == 2 ? "INTERVALS" : x == 3 ? "THC_PRESETS" : x == 4 ? "ACTIONS" : "UNKNOWN")
+
 typedef struct
 {
     uint8_t *data;
@@ -22,6 +25,8 @@ typedef struct
     uint16_t low;
     uint16_t high;
 } range_t;
+
+payload_id_t payload_get_id(const char *hex_str);
 
 payload_uplink_t payload_pack_thc(uint8_t flags, int16_t temperature, uint8_t humidity, uint16_t co2);
 
