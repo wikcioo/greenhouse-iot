04cfa5b7c6e4ce5bc30d538471d669edb2d25221
Wael Haded <49494281+wael85@users.noreply.github.com>
2023-04-24 15:25:01 +0200

    Create humidity and temperature header file

---
+++ include/humidity_temperature.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool     hum_temp_init();
+bool     hum_temp_measure();
+void     hum_temp_destroy();
+uint16_t get_last_humidity_measurement();
+uint16_t get_last_temperature_measurement();
