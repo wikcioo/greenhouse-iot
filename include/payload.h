031fdc1077ef1afba733c550acd0e0fb010d86f0
Viktor Ciolkowski <wikcioo@gmail.com>
2023-04-24 20:41:16 +0200

    Implement packing thc payload

---
+++ include/payload.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum
+{
+    THC_READINGS = 1,
+    INTERVALS    = 2,
+    THC_PRESETS  = 3,
+    ACTIONS      = 4,
+} payload_id_t;
+
+typedef struct
+{
+    uint8_t *data;
+    uint8_t  length;
+} payload_uplink_t;
+
+payload_uplink_t payload_pack_thc(uint8_t flags, int16_t temperature, uint8_t humidity, uint16_t co2);
