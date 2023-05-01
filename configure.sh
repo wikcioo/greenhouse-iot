79bc5912542737cf2d4474ad8a00880142c27f75
Viktor Ciolkowski <wikcioo@gmail.com>
2023-05-01 18:19:01 +0200

    Modify configure script to create env.h instead of exporting env variables

--- configure.sh
+++ configure.sh
@@ -3,10 +3,14 @@
 # Info: This script is used to configure Debian Linux environment in both
 #       cli mode with -c flag or user mode with no flags
 # Actions:
-#   Export environment variables used by LoRaWAN from the .env file (user mode)
+#   Create env.h header file containing LoRaWAN keys
 #   Install required binaries:
-#   gcc-avr avr-libc make libgtest-dev bear (cli mode)
-#   gcc-avr avr-libc make libgtest-dev bear gdb-avr avrdude avarice (user mode)
+#   Cli Mode:
+#       gcc-avr avr-libc make libgtest-dev
+#
+#   User Mode:
+#       gcc-avr avr-libc make libgtest-dev
+#       gdb-avr avrdude avarice bear minicom clang-format
 
 cli_mode=false
 
@@ -21,38 +25,14 @@ while getopts ":c" opt; do
     esac
 done
 
-if [[ "$cli_mode" = false && ! -f ".env" ]]; then
+env_header_file="include/env.h"
+
+if [[ "$cli_mode" = false && ! -f "$env_header_file" ]]; then
     # Export environment variables from the .env file
-    echo "File '.env' not found"
+    echo "File '$env_header_file' not found"
     read -p "Enter AppKey: " app_key
     read -p "Enter AppEUI: " app_eui
-    echo -e "AppKey=${app_key}\nAppEUI=${app_eui}" >>.env
-fi
-
-if [[ "$cli_mode" = false ]]; then
-    file_content=$(cat .env)
-    username=$(whoami)
-
-    # Check if the file is not empty
-    if [ ! -z "$file_content" ]; then
-        while read -r line; do
-            # Simple input validation
-            if [[ "$line" =~ ^[a-zA-Z0-9\=]+$ ]]; then
-                key="${line%=*}"
-                value="${line#*=}"
-
-                if [[ ! -v "$key" ]]; then
-                    export_cmd="export $key=\"$value\""
-                    export_to_rc_cmd="echo '$export_cmd' >> /home/$username/.bashrc"
-                    eval "$export_to_rc_cmd"
-                fi
-            else
-                echo "The env variable \"$line\" contains invalid character(s)"
-            fi
-        done <<<"$file_content"
-    else
-        echo "The .env file is empty"
-    fi
+    echo -e "#define LORA_appKEY \"${app_key}\"\n#define LORA_appEUI \"${app_eui}\"" >>$env_header_file
 fi
 
 # List of base packages to check for
@@ -101,8 +81,4 @@ if [ ${#missing[@]} -ne 0 ]; then
     fi
 else
     echo "All required packages are installed"
-fi
-
-if [ "$cli_mode" = false ]; then
-    exec bash
 fi
