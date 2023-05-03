31f6e47d3046214bc832dfa1d92369209adc7d38
wiktor <wikcioo@gmail.com>
2023-05-03 20:37:53 +0200

    Clean main to only initialise system and start scheduler

--- src/main.c
+++ src/main.c
@@ -1,145 +1,32 @@
-/*
- * main.c
- * Author : IHA
- *
- * Example main file including LoRaWAN setup
- * Just for inspiration :)
- */
-
 #include <ATMEGA_FreeRTOS.h>
 #include <avr/io.h>
+#include <lora_driver.h>
+#include <rc_servo.h>
 #include <semphr.h>
 #include <serial.h>
+#include <status_leds.h>
 #include <stdio.h>
 #include <stdio_driver.h>
 #include <task.h>
 
 #include "co2.h"
+#include "hardware_controller.h"
 #include "humidity_temperature.h"
+#include "lorawan.h"
 
-// Needed for LoRaWAN
-#include <lora_driver.h>
-#include <status_leds.h>
+MessageBufferHandle_t upLinkMessageBufferHandle;
+MessageBufferHandle_t downLinkMessageBufferHandle;
 
-// define two Tasks
-void task1(void *pvParameters);
-void task2(void *pvParameters);
-
-// define semaphore handle
-SemaphoreHandle_t xTestSemaphore;
-
-// Prototype for LoRaWAN handler
-void lora_handler_initialise(UBaseType_t lora_handler_task_priority);
-
-/*-----------------------------------------------------------*/
-void create_tasks_and_semaphores(void)
-{
-    // Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
-    // because it is sharing a resource, such as the Serial port.
-    // Semaphores should only be used whilst the scheduler is running, but we can set it up here.
-    if (xTestSemaphore == NULL)  // Check to confirm that the Semaphore has not already been created.
-    {
-        xTestSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore.
-        if ((xTestSemaphore) != NULL)
-        {
-            xSemaphoreGive((xTestSemaphore));  // Make the mutex available for use, by initially "Giving" the Semaphore.
-        }
-    }
-
-    xTaskCreate(
-        task1, "Task1"  // A name just for humans
-        ,
-        configMINIMAL_STACK_SIZE  // This stack size can be checked & adjusted by reading the Stack Highwater
-        ,
-        NULL, 2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-        ,
-        NULL);
-
-    xTaskCreate(
-        task2, "Task2"  // A name just for humans
-        ,
-        configMINIMAL_STACK_SIZE  // This stack size can be checked & adjusted by reading the Stack Highwater
-        ,
-        NULL, 1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-        ,
-        NULL);
-}
-
-/*-----------------------------------------------------------*/
-void task1(void *pvParameters)
-{
-    TickType_t       xLastWakeTime;
-    const TickType_t xFrequency = 500 / portTICK_PERIOD_MS;  // 500 ms
-
-    // Initialise the xLastWakeTime variable with the current time.
-    xLastWakeTime = xTaskGetTickCount();
-
-    for (;;)
-    {
-        if (hum_temp_measure())
-        {
-            puts("measure is successfully read");
-            printf("hum: %u , temp: %u \n", get_last_humidity_measurement(), get_last_temperature_measurement());
-        }
-        else
-        {
-            puts("Can't read humidity and temperature measurements");
-        }
-        xTaskDelayUntil(&xLastWakeTime, xFrequency);
-        PORTA ^= _BV(PA0);
-    }
-}
-
-/*-----------------------------------------------------------*/
-void task2(void *pvParameters)
-{
-    TickType_t       xLastWakeTime;
-    const TickType_t xFrequency = 1000 / portTICK_PERIOD_MS;  // 1000 ms
-
-    // Initialise the xLastWakeTime variable with the current time.
-    xLastWakeTime = xTaskGetTickCount();
-
-    for (;;)
-    {
-        xTaskDelayUntil(&xLastWakeTime, xFrequency);
-        if (co2_measure())
-        {
-            printf("Co2 read = %u\n", co2_get_latest_measurement());
-        }
-        else
-        {
-            printf("Error reading co2\n");
-        }
-
-        PORTA ^= _BV(PA7);
-    }
-}
-
-/*-----------------------------------------------------------*/
 void initialiseSystem()
 {
-    // Set output ports for leds used in the example
     DDRA |= _BV(DDA0) | _BV(DDA7);
 
-    // Make it possible to use stdio on COM port 0 (USB) on Arduino board - Setting 57600,8,N,1
     stdio_initialise(ser_USART0);
-    // Let's create some tasks
-    create_tasks_and_semaphores();
+    status_leds_initialise(5);
 
-    // vvvvvvvvvvvvvvvvv BELOW IS LoRaWAN initialisation vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    // Status Leds driver
-    status_leds_initialise(5);  // Priority 5 for internal task
-    // Initialise the LoRaWAN driver without down-link buffer
-    lora_driver_initialise(1, NULL);
-    // Create LoRaWAN task and start it up with priority 3
-    lora_handler_initialise(3);
-}
+    downLinkMessageBufferHandle = xMessageBufferCreate(sizeof(lora_driver_payload_t));
+    upLinkMessageBufferHandle   = xMessageBufferCreate(20);
 
-/*-----------------------------------------------------------*/
-int main(void)
-{
-    initialiseSystem();  // Must be done as the very first thing!!
-    printf("Program Started!!\n");
     if (hum_temp_init())
     {
         puts("HIH8120 driver initialized successfully");
@@ -151,9 +38,20 @@ int main(void)
 
     co2_init();
 
-    vTaskStartScheduler();  // Initialise and run the freeRTOS scheduler. Execution should never return from here.
+    rc_servo_initialise();
+    hc_handler_initialise(4);
 
-    /* Replace with your application code */
+    lora_driver_initialise(1, downLinkMessageBufferHandle);
+    lora_handler_initialise(3, 4);
+}
+
+int main(void)
+{
+    initialiseSystem();
+    printf("Program Started!!\n");
+
+    vTaskStartScheduler();
+
     while (1)
     {
     }
