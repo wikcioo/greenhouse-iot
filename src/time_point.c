2b60e49fedc66abd4d08c385682e4d57554f944c
Viktor Ciolkowski <wikcioo@gmail.com>
2023-05-13 23:58:53 +0200

    Add new time functions

--- src/time_point.c
+++ src/time_point.c
@@ -1,5 +1,16 @@
 #include "time_point.h"
 
+bool time_is_before(time_point_t *before, time_point_t *after)
+{
+    if (before->hour < after->hour)
+        return true;
+
+    if (before->hour == after->hour)
+        return before->minute < after->minute;
+
+    return false;
+}
+
 bool time_is_between(time_point_t *time, time_point_t *start, time_point_t *end)
 {
     if (time->hour < start->hour || time->hour > end->hour)
@@ -12,4 +23,23 @@ bool time_is_between(time_point_t *time, time_point_t *start, time_point_t *end)
         return false;
 
     return true;
+}
+
+uint16_t time_get_diff_in_minutes(time_point_t *t1, time_point_t *t2)
+{
+    if (!time_is_before(t1, t2))
+    {
+        // swap t1 and t2
+        time_point_t *temp = t1;
+
+        t1 = t2;
+        t2 = temp;
+    }
+
+    if (t1->hour == t2->hour)
+    {
+        return t2->minute - t1->minute;
+    }
+
+    return (60 - t1->minute) + ((t2->hour - (t1->hour + 1)) * 60) + t2->minute;
 }
