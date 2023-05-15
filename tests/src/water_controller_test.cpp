6c71feacffb66e01a39466825595510fac23976e
darkogoluza <darkogoluza@gmail.com>
2023-05-15 14:55:53 +0200

    Create water controller tests

--- tests/src/water_controller_test.cpp
+++ tests/src/water_controller_test.cpp
@@ -26,3 +26,37 @@ class WaterController : public ::testing::Test
 
     void TearDown() override {}
 };
+
+
+TEST_F(WaterController, water_controller_init)
+{
+    water_controller_init();
+
+    ASSERT_EQ(rc_servo_initialise_fake.call_count, 1);
+    ASSERT_EQ(water_valve_state, false);
+}
+
+TEST_F(WaterController, water_controller_on)
+{
+    water_controller_on();
+
+    ASSERT_EQ(rc_servo_setPosition_fake.call_count, 1);
+    ASSERT_EQ(water_valve_state, true);
+}
+
+TEST_F(WaterController, water_controller_off)
+{
+    water_controller_off();
+
+    ASSERT_EQ(rc_servo_setPosition_fake.call_count, 1);
+    ASSERT_EQ(water_valve_state, false);
+}
+
+TEST_F(WaterController, water_controller_get_state)
+{
+    water_valve_state = true;
+
+    bool result = water_controller_get_state();
+
+    ASSERT_EQ(water_valve_state, true);
+}
