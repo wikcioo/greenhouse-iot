8c7146914accf0d13702fe4ef649c26c21509228
wael85 <dr.wael85@gmail.com>
2023-05-18 14:02:04 +0200

    Remove unnecessary printf and leds

--- src/lorawan.c
+++ src/lorawan.c
@@ -1,5 +1,6 @@
 #include "lorawan.h"
 
+#include <event_groups.h>
 #include <lora_driver.h>
 #include <status_leds.h>
 #include <stddef.h>
@@ -8,13 +9,14 @@
 
 #include "env.h"
 #include "hardware_controller.h"
-#include "payload.h"
+#include "scheduler.h"
 
 extern MessageBufferHandle_t upLinkMessageBufferHandle;
 extern MessageBufferHandle_t downLinkMessageBufferHandle;
 extern MessageBufferHandle_t intervalDataMessageBufferHandle;
 extern MessageBufferHandle_t presetDataMessageBufferHandle;
 extern EventGroupHandle_t    xCreatedEventGroup;
+extern action_t              manual_watering_action;
 
 void lora_handler_initialise(UBaseType_t uplink_priority, UBaseType_t downlink_priority)
 {
@@ -69,13 +71,14 @@ void downlink_handler_task_run(void)
 
     xMessageBufferReceive(downLinkMessageBufferHandle, &downlinkPayload, sizeof(lora_driver_payload_t), portMAX_DELAY);
 
-    printf("DOWN LINK: from port: %d with %d bytes received!\n", downlinkPayload.portNo, downlinkPayload.len);
+    // printf("DOWN LINK: from port: %d with %d bytes received!\n", downlinkPayload.portNo, downlinkPayload.len);
 
     payload_id_t payload_id = payload_get_id_u8_ptr(downlinkPayload.bytes);
-    printf("Payload name = %s\n", PAYLOAD_ID_TO_NAME(payload_id));
+    // printf("Payload name = %s\n", PAYLOAD_ID_TO_NAME(payload_id));
 
     if (payload_id == ACTIONS)
     {
+        payload_unpack_actions_u8_ptr(downlinkPayload.bytes, &manual_watering_action);
         xEventGroupSetBits(xCreatedEventGroup, BIT_0);
     }
     else if (payload_id == INTERVALS)
@@ -108,38 +111,16 @@ void lora_setup(void)
 {
     char                     _out_buf[20];
     lora_driver_returnCode_t rc;
-    status_leds_slowBlink(led_ST2);  // OPTIONAL: Led the green led blink slowly while we are setting up LoRa
 
-    // Factory reset the transceiver
-    printf("FactoryReset >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_rn2483FactoryReset()));
-
-    // Configure to EU868 LoRaWAN standards
-    printf("Configure to EU868 >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_configureToEu868()));
-
-    // Get the transceivers HW EUI
+    lora_driver_rn2483FactoryReset();
+    lora_driver_configureToEu868();
     rc = lora_driver_getRn2483Hweui(_out_buf);
-    printf("Get HWEUI >%s<: %s\n", lora_driver_mapReturnCodeToText(rc), _out_buf);
+    lora_driver_setDeviceIdentifier(_out_buf);
+    lora_driver_setOtaaIdentity(LORA_appEUI, LORA_appKEY, _out_buf);
+    lora_driver_saveMac();
+    lora_driver_setAdaptiveDataRate(LORA_ON);
+    lora_driver_setReceiveDelay(500);
 
-    // Set the HWEUI as DevEUI in the LoRaWAN software stack in the transceiver
-    printf(
-        "Set DevEUI: %s >%s<\n", _out_buf, lora_driver_mapReturnCodeToText(lora_driver_setDeviceIdentifier(_out_buf)));
-
-    // Set Over The Air Activation parameters to be ready to join the LoRaWAN
-    printf(
-        "Set OTAA Identity appEUI:%s appKEY:%s devEUI:%s >%s<\n", LORA_appEUI, LORA_appKEY, _out_buf,
-        lora_driver_mapReturnCodeToText(lora_driver_setOtaaIdentity(LORA_appEUI, LORA_appKEY, _out_buf)));
-
-    // Save all the MAC settings in the transceiver
-    printf("Save mac >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_saveMac()));
-
-    // Enable Adaptive Data Rate
-    printf(
-        "Set Adaptive Data Rate: ON >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_setAdaptiveDataRate(LORA_ON)));
-
-    // Set receiver window1 delay to 500 ms - this is needed if down-link messages will be used
-    printf("Set Receiver Delay: %d ms >%s<\n", 500, lora_driver_mapReturnCodeToText(lora_driver_setReceiveDelay(500)));
-
-    // Join the LoRaWAN
     uint8_t maxJoinTriesLeft = 10;
 
     do
@@ -149,7 +130,6 @@ void lora_setup(void)
 
         if (rc != LORA_ACCEPTED)
         {
-            status_leds_longPuls(led_ST1);
             vTaskDelay(pdMS_TO_TICKS(5000UL));
         }
         else
@@ -164,9 +144,6 @@ void lora_setup(void)
     }
     else
     {
-        status_leds_ledOff(led_ST2);
-        status_leds_fastBlink(led_ST1);
-
         while (1)
         {
             taskYIELD();
