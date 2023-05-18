e65ee02782666fb0f6546114a4574605fb85b9a6
wael85 <dr.wael85@gmail.com>
2023-05-18 14:32:37 +0200

    Edit  water toggle to use only the manual toggle

--- include/scheduler.h
+++ include/scheduler.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <ATMEGA_FreeRTOS.h>
-
+#include <event_groups.h>
+#include <stdbool.h>
 #include "payload.h"
 
 #define MAX_INTERVALS 7
+#define BIT_0 (1 << 0)
 
 typedef struct
 {
@@ -18,8 +20,12 @@ typedef struct
     uint8_t index;
 } daily_time_interval_info_t;
 
-void scheduler_handler_initialise(UBaseType_t data_receive_priority, UBaseType_t scheduler_priority);
+void scheduler_handler_initialise(
+    UBaseType_t data_receive_priority, UBaseType_t scheduler_priority, UBaseType_t toggle_priority);
 void scheduler_receive_data_handler_task(void *pvParameters);
 void scheduler_schedule_events_handler_task(void *pvParameters);
 void scheduler_receive_data_handler_task_run(void);
 void scheduler_schedule_events_handler_task_run(void);
+// Manuall toggling
+void scheduler_manual_toggling_handler_task_run(void);
+void scheduler_manual_toggling_handler_task(void *pvParameters);
