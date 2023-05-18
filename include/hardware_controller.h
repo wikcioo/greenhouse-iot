5ef87403f89cdb8e37aa303fe811286fc5d292b4
wael85 <dr.wael85@gmail.com>
2023-05-18 13:52:01 +0200

    Remove unused imports and definitions from HC

--- include/hardware_controller.h
+++ include/hardware_controller.h
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <ATMEGA_FreeRTOS.h>
-#include <event_groups.h>
 #include <stdbool.h>
 
 #include "payload.h"
-
-#define BIT_0 (1 << 0)
 
 typedef struct
 {
@@ -26,8 +23,5 @@ typedef struct
 void hc_receive_preset_data_handler_task(void *pvParameters);
 void hc_toggle_handler_task(void *pvParameters);
 void hc_handler_task(void *pvParameters);
-void hc_handler_initialise(
-    UBaseType_t preset_data_receive_priority, UBaseType_t measurement_priority, UBaseType_t toggle_priority);
-void hc_receive_preset_data_handler_task_run(void);
-void hc_toggle_handler_task_run(void);
+void hc_handler_initialise(UBaseType_t preset_data_receive_priority, UBaseType_t measurement_priority);
 void hc_handler_task_run(uint8_t counter);
