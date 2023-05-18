15b833ee9ddbae475f52ccd500f0e55697776fd7
wael85 <dr.wael85@gmail.com>
2023-05-18 14:12:58 +0200

    Add methods gets sum times and time with minutes

--- src/time_point.c
+++ src/time_point.c
@@ -35,11 +35,28 @@ uint16_t time_get_diff_in_minutes(time_point_t *t1, time_point_t *t2)
         t1 = t2;
         t2 = temp;
     }
-
     if (t1->hour == t2->hour)
     {
         return t2->minute - t1->minute;
     }
 
     return (60 - t1->minute) + ((t2->hour - (t1->hour + 1)) * 60) + t2->minute;
+}
+time_point_t time_get_time_from_minutes(uint16_t minutes_count)
+{
+    return (time_point_t){.hour = minutes_count / 60, .minute = minutes_count % 60};
+}
+
+time_point_t time_get_sum_of_time_with_minutes(time_point_t t1, uint16_t minutes_of_duration)
+{
+    time_point_t t2          = {.hour = minutes_of_duration / 60, .minute = minutes_of_duration % 60};
+    uint8_t      sum_minutes = t1.minute + t2.minute;
+    if (sum_minutes >= 60)
+    {
+        return (time_point_t){.hour = t1.hour + t2.hour + 1, .minute = sum_minutes - 60};
+    }
+    else
+    {
+        return (time_point_t){.hour = t1.hour + t2.hour, .minute = sum_minutes};
+    }
 }
